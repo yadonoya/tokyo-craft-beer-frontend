9e0b61f0d8a7715c871f69a4081cae29d1a52584
yadonoya <mscottschreiber@gmail.com>
2022-05-21 11:41:53 +0900

    feat: update basic code tree. add header component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import axios from 'axios';
-import './App.css';
+import Header from './components/Header';
 
 function App() {
 
@@ -16,21 +16,8 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <Header />
         <button onClick={handleClickTest}>TEST</button>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="App-header">
+      <h2>Tokyo Craft Beer Locator</h2>
+    </div>
+  );
+};
+
+export default Header;
