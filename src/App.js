5059030d66562c1a71f6ca7ee155959339e9bd33
yadonoya <mscottschreiber@gmail.com>
2022-05-21 11:03:40 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const handleClickTest = () => {
+    console.log("inside click: ");
+    axios.get('https://tokyo-craft-beer-locations.herokuapp.com/api/bars/all_bars')
+    .then((res) => {
+      console.log("AXIOS GET", res.data);
+    })
+  }
+
+  // console.log("API TEST: ", )
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={handleClickTest}>TEST</button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
